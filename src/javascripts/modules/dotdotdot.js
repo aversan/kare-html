ddc2d264977cb3626ff52fe2c467b863c861676c
Александр Аверьянов <aversanio@gmail.com>
2017-11-15 10:17:36 +0300

    up

--- src/javascripts/modules/dotdotdot.js
+++ src/javascripts/modules/dotdotdot.js
@@ -7,11 +7,12 @@ export default class DotDotDot {
     $(this.el).each(function () {
       const $wrapper = $(this);
       const height = $wrapper.data('height') || 100;
+      const ellipsis = $wrapper.data('ellipsis') === undefined ? '\u2026' : $wrapper.data('ellipsis');
       const $text = $wrapper.find('.js-text');
       const $toggle = $wrapper.find('.js-toggle');
 
       const options = {
-        ellipsis: '\u2026',
+        ellipsis: ellipsis,
         wrap: 'word',
         watch: true,
         height,
@@ -19,7 +20,9 @@ export default class DotDotDot {
 
       $text.dotdotdot(options);
 
-      $toggle.on('click', () => {
+      $toggle.on('click', (e) => {
+        e.preventDefault();
+        
         if ($wrapper.hasClass('dotdotdot_full-text')) {
           $wrapper.removeClass('dotdotdot_full-text');
           $text.dotdotdot(options);
