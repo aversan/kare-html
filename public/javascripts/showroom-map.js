7b04729880de450a57d57c99be5d64b3fc4a6ff3
Alexandr Averyanov <aversanio@gmail.com>
2018-03-15 16:49:45 +0300

    up

--- public/javascripts/showroom-map.js
+++ public/javascripts/showroom-map.js
@@ -1,4 +1,4 @@
-var markers = {};
+const markers = {};
 
 function showroomMapHelpers() {
   const ballonHTML = ({ PREVIEW_PIC, ADDRESS, LINK }) => `
@@ -30,8 +30,8 @@ function showroomMapHelpers() {
         hintContent,
         balloonContent: ballonHTML(showroom),
         title: name,
-        id: id,
-        region: region,
+        id,
+        region,
       }, {
         iconLayout: 'default#image',
         iconImageHref,
@@ -45,61 +45,62 @@ function showroomMapHelpers() {
 function initPartnersMap() {
   const myMap = new ymaps.Map('partners-map', { center: [55.76, 37.64], zoom: 4, controls: ['zoomControl', 'fullscreenControl'] });
   const helpers = showroomMapHelpers();
-  var myPointsCollection = new ymaps.GeoObjectCollection();
+  let myPointsCollection = new ymaps.GeoObjectCollection();
+
   function setMarkers() {
     for (let i = 0; i < SHOWROOMS.length; i++) {
-      var showroom = SHOWROOMS[i];
-      var marker = helpers.marker(showroom);
-      markers[showroom.ID] = marker;
-      myPointsCollection.add(marker);
+      const showroom = SHOWROOMS[i];
+      const marker = helpers.marker(showroom);
 
-      $('.js-partner[data-id='+showroom.ID+']').on('click', function(){
-        // for (var key in markers) {
-        //   markers[key].setIcon(image);
-        // }
-        var currentID = $(this).attr('data-id');
-        // markers[currentID].setIcon(imageActive);
-        myMap.setZoom(15);
+      myPointsCollection.add(marker);
+      markers[showroom.ID] = marker;
+
+      $(`.js-partner[data-id=${showroom.ID}]`).on('click', function () {
+        const currentID = $(this).attr('data-id');
         myMap.setCenter(markers[currentID].geometry._coordinates);
+        myMap.setZoom(15, { checkZoomRange: true });
       });
     }
-    myMap.geoObjects.add(myPointsCollection);
-    myMap.setBounds(myPointsCollection.getBounds());
-  }
-  setMarkers();
-  myMap.setBounds(myMap.geoObjects.getBounds());
 
-  $('.js-choose-region').on('click', function(){
-    var region = $(this).attr('data-region');
+    myMap.geoObjects.add(myPointsCollection);
+    myMap.setBounds(myPointsCollection.getBounds(), { checkZoomRange: true });
+  }
+
+  setMarkers();
+
+  $('.js-choose-region').on('click', function () {
+    const region = $(this).attr('data-region');
 
     $('.js-partner').hide();
     $('.js-hidden-part').attr('style', '');
     $('.js-caret').removeClass('active');
 
     if (region) {
-      $('.js-partner[data-region='+region+']').show();
+      $(`.js-partner[data-region=${region}]`).show();
     } else {
       $('.js-partner').show();
-    };
+    }
 
     myPointsCollection = new ymaps.GeoObjectCollection();
-    for (var key in markers) {
+
+    for (const key in markers) {
       if (region === '' || markers[key].properties._data.region === region) {
         myPointsCollection.add(markers[key]);
       }
     }
+
     myMap.geoObjects.add(myPointsCollection);
-    myMap.setBounds(myPointsCollection.getBounds());
+    myMap.setBounds(myPointsCollection.getBounds(), { checkZoomRange: true });
   });
 
-  $('.js-partner').on('click', function(){
-    if(!$(this).hasClass('active')) {
+  $('.js-partner').on('click', function () {
+    if (!$(this).hasClass('active')) {
       $('.js-partner').removeClass('active');
       $(this).addClass('active');
     }
   });
-  
-  $('.js-caret').on('click', function(){
+
+  $('.js-caret').on('click', function () {
     $(this).siblings('.js-hidden-part').slideToggle();
     $(this).toggleClass('active');
   });
@@ -108,38 +109,41 @@ function initPartnersMap() {
 function initShowroomsMap() {
   const myMap = new ymaps.Map('showrooms-map', { center: [55.76, 37.64], zoom: 4, controls: ['zoomControl', 'fullscreenControl'] });
   const helpers = showroomMapHelpers();
-  var myPointsCollection = new ymaps.GeoObjectCollection();
+  let myPointsCollection = new ymaps.GeoObjectCollection();
+
   function setMarkers() {
     for (let i = 0; i < SHOWROOMS.length; i++) {
-      var showroom = SHOWROOMS[i];
-      var marker = helpers.marker(showroom);
+      const showroom = SHOWROOMS[i];
+      const marker = helpers.marker(showroom);
       markers[showroom.ID] = marker;
       myPointsCollection.add(marker);
     }
     myMap.geoObjects.add(myPointsCollection);
-    myMap.setBounds(myPointsCollection.getBounds());
+    myMap.setBounds(myPointsCollection.getBounds(), { checkZoomRange: true });
   }
-  setMarkers();
-  myMap.setBounds(myMap.geoObjects.getBounds());
 
-  $('.js-select-region').on('change', function(){
-    var region = $(this).val();
+  setMarkers();
+
+  $('.js-select-region').on('change', function () {
+    const region = $(this).val();
 
     if (region) {
       $('.js-partner').hide();
-      $('.js-partner[data-region='+region+']').show() 
+      $(`.js-partner[data-region=${region}]`).show();
     } else {
       $('.js-partner').show();
-    };
+    }
 
     myPointsCollection = new ymaps.GeoObjectCollection();
-    for (var key in markers) {
+
+    for (const key in markers) {
       if (region === '' || markers[key].properties._data.region === region) {
         myPointsCollection.add(markers[key]);
       }
     }
+
     myMap.geoObjects.add(myPointsCollection);
-    myMap.setBounds(myPointsCollection.getBounds());
+    myMap.setBounds(myPointsCollection.getBounds(), { checkZoomRange: true });
   });
 }
 
@@ -147,31 +151,38 @@ function initShowroomMap() {
   const myMap = new ymaps.Map('showroom-map', { center: [+SHOWROOM.COORDINATES.lat, +SHOWROOM.COORDINATES.long], zoom: 15, controls: ['zoomControl', 'fullscreenControl'] });
   const helpers = showroomMapHelpers();
 
-  function setMarkers() {
-    myMap.behaviors.disable('scrollZoom');
-    myMap.geoObjects.add(helpers.marker(SHOWROOM));
+  function setMarker() {
+    const marker = helpers.marker(SHOWROOM);
+
+    myMap.geoObjects.add(marker);
+    myMap.setZoom(15, { checkZoomRange: true });
   }
-  setMarkers();
-  myMap.setBounds(myMap.geoObjects.getBounds());
-  myMap.setZoom(15, { checkZoomRange: true });
+
+  setMarker();
 }
 
 function initPopupShowroomsMap() {
   const myMap = new ymaps.Map('popup-showrooms-map', { center: [55.76, 37.64], zoom: 4, controls: ['zoomControl', 'fullscreenControl'] });
   const helpers = showroomMapHelpers();
   const menuContainer = $('#popup-showrooms-nav');
+  const myPointsCollection = new ymaps.GeoObjectCollection();
 
   function setMarkers() {
     for (let i = 0; i < POPUP_SHOWROOMS.length; i++) {
-      myMap.behaviors.disable('scrollZoom');
-      myMap.geoObjects.add(helpers.marker(POPUP_SHOWROOMS[i]));
+      const showroom = POPUP_SHOWROOMS[i];
+      const marker = helpers.marker(showroom);
+
+      myPointsCollection.add(marker);
     }
+
+    myMap.geoObjects.add(myPointsCollection);
+    myMap.setBounds(myPointsCollection.getBounds(), { checkZoomRange: true });
   }
+
   setMarkers();
-  myMap.setBounds(myMap.geoObjects.getBounds());
 
   for (let i = 0; i < POPUP_SHOWROOMS.length; i++) {
-    let showroom = POPUP_SHOWROOMS[i];
+    const showroom = POPUP_SHOWROOMS[i];
     (function (name, city, address, count, lat, long) {
       $(`
         <a class="nav-link popup-nav-link d-flex flex-column" href="#">
@@ -180,9 +191,8 @@ function initPopupShowroomsMap() {
               <span class="text-muted text-nowrap">${count} шт.</span>
           </span>
           <span class="text-gray typo-sm">${city}, ${address}</span>
-        </a>`
-      )
-        .on('click', function() {
+        </a>`)
+        .on('click', function () {
           menuContainer.find('a').removeClass('active');
           $(this).addClass('active');
           myMap.panTo([lat, long], { flying: 1 });
